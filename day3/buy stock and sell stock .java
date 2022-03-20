ad21769b105c7a039c1ef5e4fbc076cd331c2a63
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2022-03-20 13:23:23 +0530

    Create buy stock and sell stock .java
    in this question the best time to buy stock and best time to sell stock is discuss with naive approach

---
+++ day3/buy stock and sell stock .java
@@ -0,0 +1,30 @@
+import java.util.*;
+class day3
+{ 
+    
+
+    static int maxProfit(int price[], int start, int end)
+    {
+    	if(end <= start)
+    		return 0;
+
+    	int profit = 0;
+
+    	for(int i = start; i < end; i++)
+    	{
+    		for(int j = i + 1; j <= end; j++)
+    		{
+    			if(price[j] > price[i])
+    			{
+    				int curr_profit = price[j] - price[i] 
+    								  + maxProfit(price, start, i - 1)
+    								  + maxProfit(price, j + 1, end);
+
+    				profit = Math.max(profit, curr_profit);
+    			}
+    		}
+    	}
+
+    	return profit;
+    }
+}
