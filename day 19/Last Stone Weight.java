d1de5dd5728baac9fe7ee73a8a7cf95cb55361d3
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2022-04-08 00:57:25 +0530

    Create Last Stone Weight.java

---
+++ day 19/Last Stone Weight.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+      for(int i : stones){
+            pq.add(i);
+        }
+     
+        while(pq.size() != 1){
+            pq.add(pq.remove() - pq.remove());
+        }
+        return pq.remove() ;
+    }
+}
